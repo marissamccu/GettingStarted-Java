fbc318f4c981fe1455a2319260a0a3ba723d55d2
Marissa McCubbin <marissamcc@outlook.com>
2026-08-31 10:58:31 -0400

    updated Aug 31

---
+++ New to java/src/main/java/io/github/marissamccu/Fake.java
@@ -0,0 +1,16 @@
+package io.github.marissamccu;
+
+public class Fake {
+    public int age;
+    public String firstName;
+    public String lastName;
+
+    public Fake(int age, String firstName, String lastName){
+        this.age = age;
+        this.lastName=lastName;
+
+        public int getAge(){
+            return age;
+        }
+    }
+}
--- New to java/src/main/java/io/github/marissamccu/Main.java
+++ New to java/src/main/java/io/github/marissamccu/Main.java
@@ -1,17 +1,40 @@
 package io.github.marissamccu;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.printf("i = %d%n", i);
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args){
+
+        int a =2147483647;
+        short b =32767;
+        long e = 2147483648L;
+        byte d=127;
+
+        int [] int_array ={1,2,3,5,6,7};
+        for(int i=0;i<int_array.length;i++){
+            System.out.println(int_array);
         }
+        for (int i : int_array){
+            System.out.println(i);
+        }
+
+        ArrayList<Integer> dyn_list=new ArrayList<>();
+        dyn_list.add(1);
+        dyn_list.add(2);
+        dyn_list.add(7);
+        dyn_list.add(42);
+        dyn_list.add(65);
+        dyn_list.add(67);
+
+        for (int i : dyn_list){
+            System.out.println(i);
+        }
+        for (Integer integer : dyn_list) {
+            System.out.println(integer);
+        }
+
+
+
     }
 }
